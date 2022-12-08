5a9b0297f4b7a2e27ec4ca1cc2f85d0df0fc87c0
bmil96 <bmil9696@gmail.com>
2022-12-08 22:03:15 +0100

    added routing

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hi there 😍";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController');
+Routing::get('homescreen', 'DefaultController');
+Routing::get('question', 'DefaultController');
+Routing::get('regionselect', 'DefaultController');
+Routing::run($path);
+?>
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+    public function login(){
+        $this->render('login');
+    }
+    public function homescreen(){
+        $this->render('homescreen');
+    }
+    public function question(){
+        $this->render('question');
+    }
+    public function regionselect(){
+        $this->render('regionselect');
+    }
+}
+
+?>
